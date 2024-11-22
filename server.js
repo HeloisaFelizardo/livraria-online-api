f7d1efeb72cc302f9a6c8a1654bfa096351ea603
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-11-21 23:59:17 -0300

    add: cors Render

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(
 			'http://localhost:5173',
 			'http://localhost:5174',
 			'http://localhost:5175',
+			'https://ebooks-cn24.onrender.com/',
 		],
 		methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
